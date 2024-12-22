f72513eb7d21a2d3c7f16e918361d7cb77aa631f
Kalvin Wei <kalvinwei007@gmail.com>
2024-12-23 09:06:16 +1300

    feat: add stream chat config values

---
+++ environment/chat-config.ts
@@ -0,0 +1,9 @@
+export const chatApiKey =
+  "6zw66syuaem9z8q54es9kygz5fq7j25nemgwurc4erk4vt2qrtxqhgathamk9h75";
+
+export const chatUserId = "fake-user-1";
+export const chatUserName = "Fake User Kalvin";
+
+// to inspect the JWT token, use this link: https://jwt.io/#debugger-io?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZmFrZS11c2VyLWthbHZpbiIsImV4cCI6MTczNTk0NjAwMH0.xREKaXOkNIQk8wm-rdEV68JdGVP7QzA0lxFQiwmGGOc
+export const chatUserToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZmFrZS11c2VyLWthbHZpbiIsImV4cCI6MTczNTk0NjAwMH0.xREKaXOkNIQk8wm-rdEV68JdGVP7QzA0lxFQiwmGGOc";
