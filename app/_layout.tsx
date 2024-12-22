ac1ae2da8959e2f3b2c25a3449fbcecf99b0b3e1
Kalvin Wei <kalvinwei007@gmail.com>
2024-12-23 08:53:29 +1300

    feat: add gesture support

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,22 @@
 import "react-native-gesture-handler";
+
+import { StyleSheet } from "react-native";
 import { Stack } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function Page() {
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: { backgroundColor: "green" },
-        headerTintColor: "#fff",
-        headerTitleStyle: {
-          fontWeight: "bold",
-        },
-      }}
-    >
-      <Stack.Screen name="/" options={{}} />
-    </Stack>
+    <SafeAreaProvider>
+      <GestureHandlerRootView style={styles.container}>
+        <Stack />
+      </GestureHandlerRootView>
+    </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
