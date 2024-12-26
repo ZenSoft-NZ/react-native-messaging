b4aa36ab7a22f99ab462fe4430b0ad6100a638c5
LinkunGao <gaolinkun123@gmail.com>
2024-12-26 22:58:21 +1300

    feat: add thread in channel

--- app/channel/[cid]/index.tsx
+++ app/channel/[cid]/index.tsx
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
-import React, { useEffect } from "react";
-import { Stack } from "expo-router";
+import React, { useEffect, useMemo } from "react";
+import { Stack, useRouter } from "expo-router";
 import {
   Channel,
   MessageInput,
@@ -11,9 +11,10 @@ import { useAppChatContext } from "contexts/app-context";
 import { useHeaderHeight } from "@react-navigation/elements";
 
 const ChannelScreen = () => {
-  const { channel } = useAppChatContext();
+  const { channel, setThread } = useAppChatContext();
   const { setTopInset } = useAttachmentPickerContext();
   const headerHeight = useHeaderHeight();
+  const router = useRouter();
 
   useEffect(() => {
     setTopInset(headerHeight);
@@ -27,12 +28,17 @@ const ChannelScreen = () => {
     );
   }
 
+  const handleThreadSelected = (thread) => {
+    setThread(thread);
+    router.push(`/channel/${channel.cid}/thread/${thread.cid}`);
+  };
+
   return (
     <View style={styles.container}>
       <Stack.Screen options={{ title: "Channel Screen" }} />
       {channel ? (
         <Channel channel={channel} keyboardVerticalOffset={headerHeight}>
-          <MessageList />
+          <MessageList onThreadSelect={handleThreadSelected} />
           <MessageInput />
         </Channel>
       ) : null}
