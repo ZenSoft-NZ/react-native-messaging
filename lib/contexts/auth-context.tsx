dd7e5d84cbfa92869bc1ae072bd65d6e852c5dc8
LinkunGao <gaolinkun123@gmail.com>
2025-01-04 01:13:24 +1300

    chore: add auth type, and rename jsx to tsx

---
+++ lib/contexts/auth-context.tsx
@@ -0,0 +1,42 @@
+import React, { createContext, useState, useEffect, useContext } from "react";
+import { signInWithAzureAD } from "lib/api/auth/azure";
+
+type IAuth = {
+  isAuthenticated: boolean;
+  user: {
+    id: string;
+    name: string;
+  };
+  accessToken: string;
+};
+
+const AuthContext = createContext<IAuth>({
+  isAuthenticated: false,
+  user: { id: "", name: "" },
+  accessToken: "",
+});
+
+export default function AuthProvider({ children }) {
+  const [auth, setAuth] = useState<IAuth>();
+
+  useEffect(() => {
+    async function authenticate() {
+      try {
+        const _auth = await signInWithAzureAD();
+
+        setAuth({
+          isAuthenticated: true,
+          user: _auth.user,
+          accessToken: _auth.accessToken,
+        });
+      } catch (error) {
+        console.error("Error in authenticate: ", error);
+      }
+    }
+    authenticate();
+  }, []);
+
+  return <AuthContext.Provider value={auth}>{children}</AuthContext.Provider>;
+}
+
+export const useAuth = () => useContext(AuthContext);
